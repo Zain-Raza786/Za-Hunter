d80361620209f0ae6bf6b1fa17621d7192601206
Zain Raza <zainoakgrove@gmail.com>
2021-03-12 10:49:02 -0600

    Made the app

---
+++ Za Hunter/LocationManager.swift
@@ -0,0 +1,19 @@
+//
+//  LocationManager.swift
+//  Za Hunter
+//
+//  Created by Student on 3/9/21.
+//
+
+import CoreLocation
+
+class LocationManager: NSObject, CLLocationManagerDelegate, ObservableObject {
+    var locationManager = CLLocationManager()
+    override init() {
+        super.init()
+        locationManager.delegate = self
+        locationManager.requestWhenInUseAuthorization()
+        locationManager.startUpdatingLocation()
+    }
+    
+}
